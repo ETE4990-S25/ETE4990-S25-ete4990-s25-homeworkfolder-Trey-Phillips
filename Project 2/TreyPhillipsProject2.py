6b877c627be8e9c0bc3fccf448d8d0622b86a076
Trey Phillips <treykp@gmail.com>
2025-04-04 12:12:16 -0700

    Completed threading and asynchronous. Had to fix the logic for my prime_search function so that it could work with both multiprocess and threading and it seems like it works okay. Asynchronous was really tricky to understand for me. Couldn't get good results until I implemented an "await" so that the function would wait to return the result until the prime checker was finished and got results pretty close to the example. Threading: 9,415,033 Asynchronous: 812,401

--- Project 2/TreyPhillipsProject2.py
+++ Project 2/TreyPhillipsProject2.py
@@ -12,7 +12,7 @@ def is_prime(n):
             return False
     return True
 
-def prime_search(start, step, max_time, prime_list):
+def prime_search(start, step, max_time):
     highest = 0
     num = start
     local_start = time.time()
@@ -20,23 +20,64 @@ def prime_search(start, step, max_time, prime_list):
         if is_prime(num):
             highest = num
         num += step
-    prime_list.append(highest)
+    return highest
 
 #multiprocessing
+def multiprocessing_search(start, step, max_time, result_list):
+    result = prime_search(start, step, max_time)
+    result_list.append(result)
+
 def multiprocessing_prime(max_time=180):
     cores = multiprocessing.cpu_count()
     manager = multiprocessing.Manager()
-    results = manager.list()
+    result_list = manager.list()
     processes = []
 
     for i in range(cores):
-        p = multiprocessing.Process(target=prime_search, args=(i, cores, max_time, results))
+        p = multiprocessing.Process(target=multiprocessing_search, args=(i, cores, max_time, result_list))
         processes.append(p)
         p.start()
 
     for p in processes:
         p.join()
-    return max(results) if results else 0
+
+    return max(result_list) if result_list else 0
+
+#threading
+def threaded_prime(max_time=180, thread_num=2):
+    result_list = [0]  
+    lock = threading.Lock()
+
+    def thread_search(i):
+        result = prime_search(i, thread_num, max_time)
+        with lock:
+            result_list[0] = max(result_list[0], result)
+
+    threads = []
+    for i in range(thread_num):
+        t = threading.Thread(target=thread_search, args=(i, ))
+        threads.append(t)
+        t.start()
+    for t in threads:
+        t.join()
+
+    return result_list[0]
+
+#asynchronous
+async def asynchronous_prime(max_time=180):
+    highest = 0
+    num = 0
+    start_time = time.time()
+
+    while time.time() - start_time < max_time:
+        result = await asyncio.to_thread(is_prime, num)
+        if result:
+            highest = num
+        num += 1
+
+    return highest
 
 if __name__ == "__main__":
     print("Multiprocessing:", multiprocessing_prime(180))
+    print("Threading:", threaded_prime(180))
+    print("Asynchronous:", asyncio.run(asynchronous_prime(180)))
