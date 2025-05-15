7d3a7a9dd0756c1bd30132c7bd8c146e6612b1d3
Trey Phillips <treykp@gmail.com>
2025-05-14 20:26:19 -0700

    Delete Final Project/Initializer.py

--- Final Project/Initializer.py
+++
@@ -1,8 +0,0 @@
-rates = [
-    "EUR", "GBP", "USD", "DZD", "AUD", "BWP", "BND", "CAD", "CLP", "CNY", "COP", "CZK", "DKK", "HUF", "ISK", "IDR", "ILS", "KZT", "KRW", "KWD", 
-    "LYD", "MYR", "MUR", "NPR", "NZD", "NOK", "OMR", "PKR", "PLN", "QAR", "RUB", "SAR", "SGD", "ZAR", "LKR", "SEK", "CHF", "THB", "TTD"
-]
-
-ratesForBase = [r for r in rates if r not in ("USD", "EUR", "GBP")]
-URL = "https://www.floatrates.com/historical-exchange-rates.html"
-thread_count = 10
