032badc5860e3d73fbd144db1d40d51e2ba61ffe
Trey Phillips <treykp@gmail.com>
2025-05-16 19:23:08 -0700

    Update main.py
    Changing temporarily to download the 5 major currencies and go deeper on the current EDA i am trying to perfect

--- Final Project/main.py
+++ Final Project/main.py
@@ -5,18 +5,20 @@ from initializer import ratesForBase
 from downloader import data_download, exchange_rate_print
 from utils import directory_check
 
+major_currencies = ["USD", "EUR", "GBP", "JPY", "CNY"]
 directory_check("logs")
 logging.basicConfig(filename="logs/progress.log", level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
 def main():
-    base_currency = random.choice(ratesForBase)
-    print(f"Selected base currency: {base_currency}")
+    #base_currency = random.choice(ratesForBase)
+    #print(f"Selected base currency: {base_currency}")
 
     start = datetime(2011, 5, 4)
     end = datetime.now()
 
-    data_download(base_currency, start, end)
-    exchange_rate_print(base_currency)
+    for base_currency in major_currencies:
+        data_download(base_currency, start, end)
+    #exchange_rate_print(base_currency)
 
 if __name__ == "__main__":
     main()
